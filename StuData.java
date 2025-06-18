86514cd75077e3f2b3333f2911f65d0be897c8b0
22701A02B9 <keshamreddyswetha@gmail.com>
2025-06-18 14:29:58 +0530

    Add files via upload

---
+++ StuData.java
@@ -0,0 +1,15 @@
+class Student{
+String name="swetha";
+int roll=2209;
+String branch="EEE";
+float cgpa=9.26f;
+}
+class StuData{
+public static void main(String[] args){
+Student s=new Student();
+System.out.println("Name is"+s.name);
+System.out.println("Roll is" +s.roll);
+System.out.println("Branch is" +s.branch);
+System.out.println("Cgpa is" +s.cgpa);
+}
+}
